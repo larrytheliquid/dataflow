cfaa6d734ba67b8fc5bb7bdd79700e2c519c63f9
Larry Diehl <larry@laptoptheliquid.(none)>
2009-04-25 01:58:46 -0400

    Threaded the producer too in the data-driven example

--- examples/data_driven.rb
+++ examples/data_driven.rb
@@ -3,11 +3,12 @@ include Dataflow
 
 local do |stream, doubles, triples, squares|
   unify stream, Array.new(5) { local {|v| v } }
+  
   Thread.new { unify doubles, stream.map {|n| n*2 } }
   Thread.new { unify triples, stream.map {|n| n*3 } }
   Thread.new { unify squares, stream.map {|n| n**2 } }  
 
-  stream.each {|x| unify x, rand(100) }
+  Thread.new { stream.each {|x| unify x, rand(100) } }
 
   puts "original: #{stream.inspect}"
   puts "doubles:  #{doubles.inspect}"
