ecef4dac03f6796c152e264eba36b3f5f4fd5d1f
Andrew <amiller@dappervision.com>
2009-04-27 17:12:52 -0400

    Got rid of merge conflicts <<<<<

--- actor.rb
+++ actor.rb
@@ -3,15 +3,9 @@ require 'port'
 module Dataflow
   class Actor < Thread
     include Dataflow
-<<<<<<< HEAD:actor.rb
     declare :port
     # An instance variable is needed for stream.
     attr_reader :stream
-=======
-    #Instance variables aren't working properly
-    #declare :stream, :port
-    attr_reader :stream, :port
->>>>>>> 2dc6c51... Corrected a typo in actor.rb:actor.rb
 
     def initialize(&block)
       @stream = Variable.new
