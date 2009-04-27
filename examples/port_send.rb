61a349660693da29a4c245bd07fb69db043f017c
Larry Diehl <larry@laptoptheliquid.(none)>
2009-04-27 18:26:18 -0400

    Changed port_send example to something more straightforward

--- examples/port_send.rb
+++ examples/port_send.rb
@@ -1,16 +1,10 @@
 require "#{File.dirname(__FILE__)}/../dataflow"
 include Dataflow
 
-local do |p, x|
-  unify p, Port.new(x)
-  p.send "1"
-  p.send 2
-  Thread.new {p.send 3}
-  Thread.new { puts "x.take 5: #{x.take 5}" }
-  p.send 4
-  p.send "5"
-  puts "x.take 1: #{x.take 1}"
+local do |port, stream|
+  unify port, Dataflow::Port.new(stream)
+  Thread.new {port.send 2}
+  Thread.new {port.send 8}
+  Thread.new {port.send 1024}
+  puts stream.take(3).sort.inspect
 end
-# Give threads a chance to complete
-sleep 0.01
-
