b5a8dc90e4959e2821adfbe7977518f2ea6bce3e
Andrew <amiller@dappervision.com>
2009-04-27 19:50:41 +0800

    Implemented ports using dataflow variables, and a single state variable. Actors now don't need to use anything except ports and dataflow.
    Signed-off-by: Larry Diehl <larrytheliquid@gmail.com>

---
+++ examples/portsend.rb
@@ -0,0 +1,15 @@
+require "#{File.dirname(__FILE__)}/../port"
+include Dataflow
+
+local do |p, x|
+  unify p, Port.new(x)
+  p.send "1"
+  p.send 2
+  Thread.new {p.send 3}
+  Thread.new { puts "x.take 5: #{x.take 5}" }
+  p.send 4
+  p.send "5"
+  puts "x.take 1: #{x.take 1}"
+end
+# Give threads a chance to complete
+sleep 0.01
---
+++ port.rb
@@ -0,0 +1,53 @@
+require 'dataflow'
+require 'thread'
+
+module Dataflow
+  class Port  
+    include Dataflow
+    LOCK = Mutex.new
+
+    class Stream
+      # This is preferable to the attr_readers and initialized, but it doesn't work
+      include Dataflow
+      #declare :tail, :head
+  
+      # This is uglier and should be replaced with the above
+      attr_reader :head, :tail
+      def initialize
+        local do |h, t|
+          @head = h
+          @tail = t
+        end
+      end
+    
+      # Defining each allows us to use the enumerable mixin
+      # None of the list can be garbage collected less the head is
+      # garbage collected, so it will grow indefinitely even though
+      # the function isn't recursive.
+      include Enumerable
+      def each
+        s = self
+        while 1
+          yield s.head
+          s = s.tail
+        end
+      end
+    end
+  
+    # Create a stream object, bind it to the input variable
+    # Instance variables are necessary because @tail is state
+    def initialize(x)
+      @tail = Stream.new
+      unify x, @tail
+    end
+  
+    # This needs to be synchronized because it uses @tail as state
+    def send value
+      LOCK.synchronize do
+        unify @tail.head, value
+        unify @tail.tail, Stream.new
+        @tail = @tail.tail
+      end
+    end
+  end
+end
