f6c9810d95c49208df57fa7037052244a8dc059a
Larry Diehl <larry@laptoptheliquid.(none)>
2009-04-28 20:17:03 -0400

    Separated out dataflow_http_gets example from port_http_get example

--- examples/dataflow_http_gets.rb
+++ examples/dataflow_http_gets.rb
@@ -5,9 +5,9 @@ include Dataflow
 # Be gentle running this one
 Thread.abort_on_exception = true
 local do |stream, branding_occurences, number_of_mentions|
-  unify stream, Array.new(100) { Dataflow::Variable.new }
+  unify stream, Array.new(10) { Dataflow::Variable.new }
   stream.each {|s| Thread.new(s) {|v| unify v, Net::HTTP.get_response(URI.parse("http://www.cuil.com/search?q=#{rand(1000)}")).body } }
   Thread.new { unify branding_occurences, stream.map {|http_body| http_body.scan /cuil/ } }
-  Thread.new { unify number_of_mentions, branding_occurences.map {|googles| googles.length } }
+  Thread.new { unify number_of_mentions, branding_occurences.map {|occurences| occurences.length } }
   puts number_of_mentions.inspect
 end
