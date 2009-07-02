e30cf99d8232f3f5918b07df2e4f811289dd5cdd
Larry Diehl <larry@laptoptheliquid.local>
2009-07-02 09:04:34 -0700

    Added ideal specs for how equality, such that Dataflow proxy variables get treated the same way regular Ruby variables do

--- spec/equality_spec.rb
+++ spec/equality_spec.rb
@@ -1,33 +1,35 @@
 require "#{File.dirname(__FILE__)}/spec_helper"
 
-describe 'Unifying a bound value to an object of equal value' do
-  describe 'for classes that define == to perform method calls on the RHS' do
-    it 'passes unification' do
-      local do |var, var2|
-        unify var, "str"
-        var.should == "str"
-        "str".should == var
-        lambda {unify var, "str"}.should_not raise_error
+describe 'Unifying a bound value' do
+  [3, 2.0, Object.new, Class.new.new, "str", :sym, /abc/].each do |type|
+    describe "for #{type.class} instances" do
+      it 'passes unification for an object of equal value' do
+        local do |var, var2|
+          unify var, type
+          var.should == type
+          type.should == var
+          lambda {unify var, type}.should_not raise_error
 
-        unify var2, "str"
-        var.should == var2
-        var2.should == var
-        lambda {unify var, var2}.should_not raise_error        
+          unify var2, type
+          var.should == var2
+          var2.should == var
+          lambda {unify var, var2}.should_not raise_error        
+        end
       end
-    end
-  end
-  
-  describe 'for classes that do not define == to perform method calls on the RHS' do    
-    it 'passes unification' do
-      local do |var, var2|
-        unify var, :sym
-        var.should == :sym
-        lambda {unify var, :sym}.should_not raise_error
 
-        unify var2, :sym
-        var.should == var2
-        var2.should == var
-        lambda {unify var, var2}.should_not raise_error
+      it 'fails unification for an object of inequal value' do
+        different = Object.new
+        local do |var, var2|
+          unify var, different
+          var.should != different
+          different.should != var
+          lambda {unify var, different}.should raise_error(Datatalow::UnificationError)
+
+          unify var2, different
+          var.should != var2
+          var2.should != var
+          lambda {unify var, var2}.should_not raise_error(Datatalow::UnificationError)
+        end
       end
     end
   end
